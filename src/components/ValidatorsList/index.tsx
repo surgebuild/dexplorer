79038f6897a9c695355510fadb88f2020fbd5418
Shreyank <shreyank@designstring.com>
2024-11-19 12:38:16 +0530

    validator listing page

--- src/components/ValidatorsList/index.tsx
+++ src/components/ValidatorsList/index.tsx
@@ -9,14 +9,21 @@ import {
   Thead,
   Tr,
 } from '@chakra-ui/react'
+import { toHex } from '@cosmjs/encoding'
+import { Validator } from 'cosmjs-types/tendermint/types/validator'
 
 import { getColor, truncate } from '@/utils'
 
 interface IValidatorsList {
   title: string
+  list: Validator[]
 }
 
-export default function ValidatorsList({ title }: IValidatorsList) {
+export default function ValidatorsList({ title, list }: IValidatorsList) {
+  const totalVotingPower = list.reduce(
+    (acc, validator) => acc + BigInt(validator.votingPower),
+    BigInt(0n)
+  )
   return (
     <Box
       pt={10}
@@ -63,48 +70,59 @@ export default function ValidatorsList({ title }: IValidatorsList) {
             </Tr>
           </Thead>
           <Tbody>
-            {validators.map((validator, ind) => (
-              <Tr
-                key={ind}
-                px={6}
-                borderBottom={'1px'}
-                borderColor={'gray-900'}
-                _last={{ borderBottom: 'none' }}
-              >
-                <Td border={'none'}>
-                  <Text className="body2_regular" color={'white'}>
-                    {validator.name}
-                  </Text>
-                </Td>
-                <Td border={'none'}>
-                  <Text
-                    px={2}
-                    py={1}
-                    borderRadius={'8px'}
-                    bg={getColor(validator.status)}
-                    className="label_regular"
-                    color={'white'}
-                    width={'max-content'}
-                    textAlign={'center'}
+            {list.map((validator, ind) => {
+              const percentage =
+                (Number(validator.votingPower) / Number(totalVotingPower)) * 100
+              return (
+                <Tr
+                  key={ind}
+                  px={6}
+                  borderBottom={'1px'}
+                  borderColor={'gray-900'}
+                  _last={{ borderBottom: 'none' }}
+                >
+                  <Td border={'none'}>
+                    <Text className="body2_regular" color={'white'}>
+                      {`Validator ${ind + 1}`}
+                    </Text>
+                  </Td>
+                  <Td border={'none'}>
+                    <Text
+                      px={2}
+                      py={1}
+                      borderRadius={'8px'}
+                      bg={getColor(1)}
+                      className="label_regular"
+                      color={'white'}
+                      width={'max-content'}
+                      textAlign={'center'}
+                    >
+                      {'Active'}
+                    </Text>
+                  </Td>
+                  <Td
+                    border={'none'}
+                    className="supportText_semibold"
+                    color={'text-50'}
+                  >{`# ${truncate(
+                    toHex(validator.address),
+                    8
+                  ).toUpperCase()}`}</Td>
+                  <Td
+                    border={'none'}
+                    className="body2_regular"
+                    color={'text-50'}
                   >
-                    {validator.status == 1 ? 'Active' : 'Inactive'}
-                  </Text>
-                </Td>
-                <Td
-                  border={'none'}
-                  className="supportText_semibold"
-                  color={'text-50'}
-                >{`# ${truncate(validator.address)}`}</Td>
-                <Td border={'none'} className="body2_regular" color={'text-50'}>
-                  {validator.votingPower}
-                </Td>
-                <Td
-                  border={'none'}
-                  className="body2_regular"
-                  color={'text-50'}
-                >{`${validator.commission}`}</Td>
-              </Tr>
-            ))}
+                    {`${percentage} %`}
+                  </Td>
+                  <Td
+                    border={'none'}
+                    className="body2_regular"
+                    color={'text-50'}
+                  >{`-`}</Td>
+                </Tr>
+              )
+            })}
           </Tbody>
         </Table>
       </TableContainer>
@@ -112,29 +130,29 @@ export default function ValidatorsList({ title }: IValidatorsList) {
   )
 }
 
-const validators = [
-  {
-    name: 'Foundation 1',
-    status: 1,
-    active: true,
-    address: '0xC09368ACD3024C7E293395ABC123456789ABCDEF',
-    votingPower: 60,
-    commission: '60%',
-  },
-  {
-    name: 'Foundation 2',
-    status: 1,
-    active: true,
-    address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-    votingPower: 30,
-    commission: '30%',
-  },
-  {
-    name: 'Foundation 3',
-    status: 1,
-    active: true,
-    address: '0x06e70f295B6337c213DDe82D13cc198027687A7B',
-    votingPower: 10,
-    commission: '10%',
-  },
-]
+// const validators = [
+//   {
+//     name: 'Foundation 1',
+//     status: 1,
+//     active: true,
+//     address: '0xC09368ACD3024C7E293395ABC123456789ABCDEF',
+//     votingPower: 60,
+//     commission: '60%',
+//   },
+//   {
+//     name: 'Foundation 2',
+//     status: 1,
+//     active: true,
+//     address: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+//     votingPower: 30,
+//     commission: '30%',
+//   },
+//   {
+//     name: 'Foundation 3',
+//     status: 1,
+//     active: true,
+//     address: '0x06e70f295B6337c213DDe82D13cc198027687A7B',
+//     votingPower: 10,
+//     commission: '10%',
+//   },
+// ]
